661a15d50826ea86c75173e74da081c5b0d8937f
taxuda <trandat.student>
2021-03-21 14:51:16 +0100

    add tests with assert

--- übungen/rechnung_erstellen(if-else)/rechnung_erstellen.h
+++ übungen/rechnung_erstellen(if-else)/rechnung_erstellen.h
@@ -5,6 +5,7 @@
 #ifndef RECHNUNG_ERSTELLEN_IF_ELSE__RECHNUNG_ERSTELLEN_H
 #define RECHNUNG_ERSTELLEN_IF_ELSE__RECHNUNG_ERSTELLEN_H
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 unsigned int rechnungErstellen(unsigned int auftragssumme);
@@ -14,8 +15,13 @@ unsigned int rechnungErstellen(unsigned int auftragsummer){
     if(auftragsummer < 300){
         verpackungszuschlag = 10;
         versandkosten = 15;
+        assert((auftragsummer < 300) && (verpackungszuschlag == 10) && (versandkosten == 15));
     }else if(auftragsummer < 1000){
         verpackungszuschlag = 5;
+        assert((auftragsummer >= 300) && (auftragsummer < 1000)
+                && (verpackungszuschlag == 5) && (versandkosten == 0));
+    }else{
+        assert((auftragsummer >= 1000) && (verpackungszuschlag == 0) && (versandkosten == 0));
     }
     cout << "Verpackungszuschlag: " << verpackungszuschlag << " Euro" << endl;
     cout << "Versandkosten: " << versandkosten << " Euro" << endl;
