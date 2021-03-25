80722b9a01e579eaa6892bbddbff3066c38d17a0
taxuda <trandat.student>
2021-03-25 15:56:52 +0100

    verbessern den Code

--- praktikum/prak04/01_Caesar-Verschluesselung/main.cpp
+++ praktikum/prak04/01_Caesar-Verschluesselung/main.cpp
@@ -2,77 +2,46 @@
 #include <string>
 using namespace std;
 
-const unsigned ANZAHL_BUCHSTABE = unsigned ('z') - unsigned ('a') + 1;
+const unsigned ALPHABET_SIZE = unsigned ('z') - unsigned ('a') + 1; // 26
 
+bool isBuchstabe(char zeichen){
 
-bool isKleineBuchstabe(char zeichen);
-bool isGroesseBuchstabe(char zeichen);
-unsigned positionInAlphabet(char buchstabe);
-unsigned moeglicheAnzahlVerschiebepostionen(unsigned anzahlVerschiebeposition);
-unsigned neuePositionInALphabetNachVerschieben(char buchstabe, unsigned anzahlVerschiebeposition);
-char verschieben(char buchstabe, unsigned anZahlVerschiebeposition);
-
-/*
- * 'a','A' <=> 0.position
- * ...
- * 'z','Z' <=> 25.position
- */
-unsigned positionInAlphabet(char buchstabe){
-    if(isKleineBuchstabe(buchstabe))
-        return unsigned(buchstabe - 'a');
-    else if(isGroesseBuchstabe(buchstabe))
-        return unsigned(buchstabe - 'A');
+    return (zeichen >= 'a' && zeichen <= 'z') || (zeichen >= 'A' && zeichen <= 'Z');
 }
 
-unsigned moeglicheAnzahlVerschiebepostionen(unsigned anzahlVerschiebeposition){
-    // Es gibt moeglich ingesamts 26 Postionen zum Verschieben
-    return anzahlVerschiebeposition % ANZAHL_BUCHSTABE;
-}
+char verschieben(char buchstabe, unsigned anzahlVerschiebenPosition){
 
-unsigned neuePositionInALphabetNachVerschieben(char buchstabe, unsigned anzahlVerschiebeposition){
-    return (positionInAlphabet(buchstabe) + moeglicheAnzahlVerschiebepostionen(anzahlVerschiebeposition)) % ANZAHL_BUCHSTABE;
-}
+    char neuBuchstabe = buchstabe + anzahlVerschiebenPosition;
 
-char verschieben(char buchstabe, unsigned anzahlVerschiebeposition){
-    unsigned neuePos = neuePositionInALphabetNachVerschieben(buchstabe, anzahlVerschiebeposition);
-    if(isKleineBuchstabe(buchstabe))
-        return ('a' + neuePos);
-    else if(isGroesseBuchstabe(buchstabe))
-        return ('A' + neuePos);
-}
-
-bool isKleineBuchstabe(char zeichen){
-    if(zeichen >= 'a' && zeichen <= 'z'){
-        return true;
+    if(!isBuchstabe(neuBuchstabe)){
+        neuBuchstabe -= ALPHABET_SIZE;
     }
-    return false;
-}
-bool isGroesseBuchstabe(char zeichen){
-    if((zeichen >= 'A' && zeichen <= 'Z')){
-        return true;
-    }
-    return false;
-}
 
-
+    return neuBuchstabe;
+}
 
 int main() {
+
     string s;
-    unsigned verschiebenKey;
+    unsigned key;
 
     // Eingabe..
     cout << "Bitte geben Sie den zu verschluesselnden Text ein: ";
     getline(cin, s);
+
     cout << "Bitte geben Sie die Anzahl Verschiebepositionen ein (als positive ganze Zahl): ";
-    cin >> verschiebenKey;
+    cin >> key;
+
+    // Anzahl mögliche Verschiebepositionen
+    unsigned anzahlVerschiebenPosition = key % ALPHABET_SIZE;
 
     // Verschluesselung..
-    for(unsigned i = 0; i >= 0 && i <= s.length() - 1; i++){
-        if(isKleineBuchstabe(s.at(i))){
-            s.at(i) = verschieben(s.at(i), verschiebenKey);
-        }else if(isGroesseBuchstabe(s.at(i))){
-            s.at(i) = verschieben(s.at(i), verschiebenKey);
-        }
+    for(unsigned i = 0; i >= 0 && i <= s.size() - 1; i++){
+
+        char zeichen = s[i];
+
+        if(isBuchstabe(zeichen))
+            s[i] = verschieben(zeichen, anzahlVerschiebenPosition);
     }
 
     cout << s << endl;
