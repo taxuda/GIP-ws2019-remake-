d8fb48f085ae6efd18ad3e7b667a7bc24a76492b
taxuda <trandat.student>
2021-03-29 21:31:39 +0200

    solved CImg by CMakeLists.txt to run on MacOs

--- praktikum/prak06/03_Animation/animation.cpp
+++ praktikum/prak06/03_Animation/animation.cpp
@@ -3,19 +3,25 @@
 #include "CImgGIP05.h"
 
 using namespace std;
+// Für das "blaue Spielfeld" ...
+const int left_border = 100;
+const int right_border = 500;
+const int rectangle_size = 50;
 
+bool isValidPostion(int x){
+    return (x >= left_border) && (x <= right_border - rectangle_size);
+}
 
 int main(){
-    // Für das "blaue Spielfeld" ...
-    const int left_border = 100;
-    const int right_border = 500;
+
 
     // Für Ausdehnung und Position des weißen Quadrats ...
     int x = 200, y = 300;
-    const int rectangle_size = 50;
     int delta = 10;
 
     gip_white_background();
+
+    bool turnRight = true;
 
     while(gip_window_not_closed()){
 
@@ -36,7 +42,12 @@ int main(){
         // blauen "Spielfelds" abprallen muss ...
 
         /* Ihr Code zur Berechnung der neuen Werte von x und y hier */
-
+        if(isValidPostion(x + delta)){
+            x += delta;
+        }else{  // change direction by changes sign of delta
+            delta = -delta;
+            x += delta;
+        }
         gip_wait(100);
     }
     return 0;
